6d7978706427023bafee60fa85635e315ba4e235
Leonid Dashko <all-cs.info@i.ua>
2017-10-22 19:31:27 +0300

    Show controller in front of camera

--- Assets/Scripts/GlueToHeadset.cs
+++ Assets/Scripts/GlueToHeadset.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GlueToHeadset : MonoBehaviour {
+    public GameObject Headset;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update() {
+        float distance = 4f;
+        transform.position = Headset.transform.position + Headset.transform.forward * distance;
+        transform.rotation = new Quaternion(0.0f, Headset.transform.rotation.y, 0.0f, Headset.transform.rotation.w);
+    }
 }
